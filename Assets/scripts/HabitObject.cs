c557c9704cc5ba422704e8bb7c9b2265276c12d6
Mary <mary.luong21@yahoo.com>
2020-04-09 04:50:06 -0400

    modified pet habit structure

---
+++ Assets/scripts/HabitObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HabitObject
+{
+    public string habit;
+    public int healvalue;
+    public int hurtvalue;
+    public string lastcared;
+    public int timeframe;
+
+    public HabitObject(string habit, int healvalue, int hurtvalue, string lastcared, int timeframe) {
+        this.habit = habit;
+        this.healvalue = healvalue;
+        this.hurtvalue = hurtvalue;
+        this.lastcared = lastcared;
+        this.timeframe = timeframe;
+    }
+}
